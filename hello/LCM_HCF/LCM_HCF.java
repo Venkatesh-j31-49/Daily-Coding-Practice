764864b7bfae0378e64cea08a6739f4a38f9d932
Venkatesh-j31-49 <156523018+Venkatesh-j31-49@users.noreply.github.com>
2024-09-14 19:47:47 +0530

    Update LCM_HCF.java

--- hello/LCM_HCF/LCM_HCF.java
+++ hello/LCM_HCF/LCM_HCF.java
@@ -14,9 +14,22 @@ public class LCM_HCF {
                 hcf = i;
             }
         }
+        /*Lcm 
+        int lcm=(a>b)?a:b;
+        while(true)
+        {
+        if(lcm%a==0 && lcm%b==0)
+        {
+        break;
+        }
+        lcm++
+        }
+        */
 
         int lcm = (a * b) / hcf;
         System.out.println("The Lcm and Hcf of two numbers is :" + lcm + " " + hcf);
+
+    
     }
 
 }
