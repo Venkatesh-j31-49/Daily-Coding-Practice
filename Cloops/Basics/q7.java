356a6088479ec9a119c7c5cf9fae13b5457f2f09
venkatesh v <venkateshvaddevani@gmail.com>
2024-09-15 22:17:35 +0530

    d

---
+++ Cloops/Basics/q7.java
@@ -0,0 +1,11 @@
+package Cloops.Basics;
+import java.util.Scanner;
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Double a=sc.nextDouble();
+        Double Area=(Math.sqrt(3)/4)*a*a;
+        System.out.println(Area);
+    }
+    
+}
---
+++ Cloops/Basics/q8.java
@@ -0,0 +1,12 @@
+package Cloops.Basics;
+import java.util.Scanner;
+public class q8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double R=sc.nextDouble();
+        double Perimeter=2*(Math.PI)*R;
+        System.out.println(Perimeter);
+        
+        }
+    
+}
