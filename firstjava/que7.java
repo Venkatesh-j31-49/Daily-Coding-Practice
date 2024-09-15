7ab694a53c90fffefc028110baeb6687b9d2a749
venkatesh v <venkateshvaddevani@gmail.com>
2024-09-15 15:12:52 +0530

    q7

---
+++ firstjava/que7.java
@@ -0,0 +1,20 @@
+package firstjava;
+import java.util.Scanner;
+public class que7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=0;
+        int n2=1;
+        for(int i=0;i<=n;i++)
+        {
+            System.out.println(n1);
+            int n3=n1+n2;
+            n1=n2;
+            n2=n3;
+
+
+        }
+    }
+    
+}
