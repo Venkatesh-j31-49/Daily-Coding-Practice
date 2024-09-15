682145afba8949b32340704dab20aa7714a9fcac
venkatesh v <venkateshvaddevani@gmail.com>
2024-09-15 21:53:20 +0530

    q2 done

---
+++ Cloops/Basics/q2.java
@@ -0,0 +1,12 @@
+package Cloops.Basics;
+import java.util.Scanner;
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int Area=(a*b)/2;
+        System.out.println(Area);
+    }
+    
+}
