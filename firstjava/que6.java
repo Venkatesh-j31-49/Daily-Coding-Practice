97300da8d30e2f6427bced22376fef34fba7fda3
venkatesh v <venkateshvaddevani@gmail.com>
2024-09-15 15:05:21 +0530

    done q6

---
+++ firstjava/que6.java
@@ -0,0 +1,11 @@
+package firstjava;
+import java.util.Scanner;
+public class que6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double R=sc.nextDouble();
+        double d=83.89;
+        double D=R/d;
+        System.out.println(R +" rupees in Dollars :"+D);
+    }
+}
