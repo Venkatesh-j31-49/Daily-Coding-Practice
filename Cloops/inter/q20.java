4fd3d48646fe21da9f4d67eb3f7c08d787f8af4b
venkatesh v <venkateshvaddevani@gmail.com>
2024-10-14 16:18:35 +0530

    dd

---
+++ Cloops/inter/q20.java
@@ -0,0 +1,18 @@
+package Cloops.inter;
+import java.util.Scanner;
+
+public class q20 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int d=sc.nextInt();
+        int n=sc.nextInt();
+        int term=a;
+        for(int i=0;i<=n;i++)
+        {
+            System.out.print(term+",");
+            term=term*d;
+        }
+    }
+    
+}
