07c8bb3b44f668e32ca23d48a448cf686e4ef416
venkatesh v <venkateshvaddevani@gmail.com>
2024-12-23 14:03:31 +0530

    dd

---
+++ Logic_B/NumberTheory/q6.java
@@ -0,0 +1,12 @@
+package Logic_B.NumberTheory;
+import java.util.*;
+public class q6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int or=n;
+        int res=0;
+        while(true)
+    }
+    
+}
--- hello/leapyr/leapy.java
+++ hello/leapyr/leapy.java
@@ -1,11 +1,12 @@
-import java.util.*;
+package hello.leapyr;
+import java.util.Scanner;
 
 public class leapy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n % 4 == 0 && (n % 100 == 0 || n % 400 == 0)) {
+        if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
 
             System.out.println(n + " is a Leap year");
         } else {
