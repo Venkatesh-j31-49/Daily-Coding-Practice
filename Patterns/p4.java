b50f45a2f34e0064deac8e50cee5e141101fd134
venkatesh v <venkateshvaddevani@gmail.com>
2024-12-23 14:03:54 +0530

    d

---
+++ Patterns/p4.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class p4 {
+    public static void main(String[] args) {
+        int c=0;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                c++;
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
